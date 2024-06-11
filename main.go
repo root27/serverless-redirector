2669b262e3f0142905fe765c3bfa80d4efa9e76f
root27 <oguzhaneee@gmail.com>
2024-06-11 15:02:47 +0300

    update

--- main.go
+++ main.go
@@ -4,9 +4,60 @@ import (
 	"context"
 	"google.golang.org/api/sheets/v4"
 	"log"
+	"net/url"
+	"strings"
+	"time"
 )
 
-// Retrieve a token, saves the token, then returns the generated client.
+type URLMap map[string]*url.URL
+
+type cached struct {
+	db        URLMap
+	updatedAt time.Time
+}
+
+func (c *cached) refreshData() error {
+
+}
+
+func GetUrls(data [][]interface{}) URLMap {
+
+	output := make(URLMap)
+
+	for _, v := range data {
+
+		key, ok := v[0].(string)
+
+		if !ok {
+
+			continue
+		}
+
+		key = strings.ToLower(key)
+
+		value, ok := v[1].(string)
+
+		if !ok {
+
+			continue
+		}
+
+		url, err := url.Parse(value)
+
+		if err != nil {
+
+			continue
+		}
+
+		output[key] = url
+
+	}
+
+	log.Println(output)
+
+	return output
+
+}
 
 func main() {
 
@@ -15,8 +66,7 @@ func main() {
 		log.Fatalf("Unable to retrieve Sheets client: %v", err)
 	}
 
-	// Prints the names and majors of students in a sample spreadsheet:
-	// https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+	//NOTE: Set env variable
 	spreadsheetId := "1HUIYUXbSX21ZgR93l0ryA-Q_PMoNGeIUlAIlHzT9dNc"
 	readRange := "A:B"
 	resp, err := srv.Spreadsheets.Values.Get(spreadsheetId, readRange).Do()
@@ -24,7 +74,11 @@ func main() {
 		log.Fatalf("Unable to retrieve data from sheet: %v", err)
 	}
 
-	log.Println(resp.Values)
+	if len(resp.Values) == 0 {
+
+		log.Println("No data available")
+
+		return
+	}
 
 }
-
